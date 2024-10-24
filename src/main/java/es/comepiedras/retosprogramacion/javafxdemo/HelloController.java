62785aacf94d6b027fdf472b486a368839df9983
Jesus García <jeigar2@gmail.com>
2024-10-24 04:23:22 +0200

    Commit inicial

---
+++ src/main/java/es/comepiedras/retosprogramacion/javafxdemo/HelloController.java
@@ -0,0 +1,14 @@
+package es.comepiedras.retosprogramacion.javafxdemo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
